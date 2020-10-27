71cfd129ad72ded6cc74cb7d2991acc8e5fc3cac
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-10-27 16:41:32 -0300

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
-setTimeout(() => {
-    alert('[Warning!] This website does not have any database, it is just for simulation')
-}, 500)
+
+
+const vw = window.innerWidth
+
+if (vw <= 1097) {
+    alert('[ERROR] Device too small')
+} else {
+    setTimeout(() => {
+        alert('[Warning!] This website does not have any database, it is just for simulation')
+    }, 500)
+}
